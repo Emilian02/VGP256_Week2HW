64b24d2baecc4d124375f94161d00f1b0bcd01ca
Emilian02 <emijurflo@gmail.com>
2024-07-20 23:18:17 -0400

    Task 2 completed

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -99,24 +99,125 @@ public:
 
 };
 
-
 //------------------Equation 2------------------
-//Solving for final velocity
-float Eq2FinalVelocity(float& vi, float& acc, float& time)
+class Equation2
 {
-    return vi + (acc * time);
-}
-//Solving for inicial velocity
-float Eq2InicialVelocity(float& vf, float& acc, float& time)
-{
-    return vf - (acc * time);
-}
-//Solving for acceleration
+public:
+    //Solving for average velocity
+    float Eq2AverageVelocity(float& vi, float& vf)
+    {
+        return (vi + vf) / 2;
+    }
+    //Solving for final velocity
+    float Eq2FinalVelocity(float& vi, float& av)
+    {
+        return (2 * av) - vi;
+    }
+    //Solving for inicial velocity
+    float Eq2InicialVelocity(float& vf, float& av)
+    {
+        return (2 * av) - vf;
+    }
+
+
+};
+
 //------------------Equation 3------------------
+class Equation3
+{
+public:
+    //Solving for displacement
+    float Eq3Displacement(float& vi, float& vf, float& time)
+    {
+        return ((vi + vf) * time) / 2;
+    }
+    //Solving for final velocity
+    float Eq3FinalVelocity(float& vi, float& x, float& time)
+    {
+        return ((2 * x) / time) - vi;
+    }
+    //Solving for inicial velocity
+    float Eq3InicialVelocity(float& vf, float& x, float& time)
+    {
+        return ((2 * x) / time) - vf;
+    }
+    //Solving for time
+    float Eq3FTime(float& vi, float& vf, float& x)
+    {
+        return (2 * x) / (vi + vf);
+    }
+};
 
 //------------------Equation 4------------------
+class Equation4
+{
+public:
+    //Solving for displacement
+    float Eq4Displacement(float& vi, float& acc, float& time)
+    {
+        return (vi * time) + (acc * pow(time, 2)) / 2;
+    }
+    //Solving for acceleration
+    float Eq4FAcceleration(float& vi, float& x, float& time)
+    {
+        return (2 * (x - (vi * time))) / pow(time, 2);
+    }
+    //Solving for inicial velocity
+    float Eq4InicialVelocity(float& acc, float& x, float& time)
+    {
+        return (x - (acc * pow(time, 2)) / 2) / time;
+    }
+    //Solving for time
+    void  Eq4FTime(float& vi, float& x, float& acc)
+    {
+        float a = 0.5f * acc;
+        float b = vi;
+        float c = x;
+
+        //Calculating discrimant
+        float discrimant = b * b - 4 * a * c;
+
+        if (discrimant < 0)
+        {
+            std::cout << "There's no real solution for this time" << std::endl;
+        }
+
+        //Calultaing the time
+        discrimant = sqrt(discrimant);
+        float t1 = (-b + discrimant) / (2 * a);
+        float t2 = (-b - discrimant) / (2 * a);
+
+        std::cout << "Possible times are: " << t1 << " and " << t2 << std::endl;
+    }
+};
 
 //------------------Equation 5------------------
+class Equation5
+{
+public:
+    //Solving for final velocity
+    float Eq5FinalVelocity(float& vi, float& acc, float& x)
+    {
+        float vf = pow(vi, 2) + (2 * acc * x);
+        return pow(vf, 2);
+    }
+    //Solving for inicial velocity
+    float Eq5InicialVelocity(float& vf, float& acc, float& x)
+    {
+        float vi = pow(vf, 2) - (2 * acc * x);
+        return pow(vi, 2);
+    }
+    //Solving for acceleration
+    float Eq5Acceleration(float& vi, float& vf, float& x)
+    {
+        return (pow(vf, 2) - pow(vi, 2)) / (2 * x);
+    }
+    //Solving for displacement
+    float Eq5Displacement(float& vi, float& acc, float& vf)
+    {
+        return (pow(vf, 2) - pow(vi, 2)) / (2 * acc);
+    }
+};
 
 int main()
 {
